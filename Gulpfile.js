2fa9a2a5840aaf96fed5c26f7e04699e14c4e4f6
Adrian von Gegerfelt <adrian@designbyadrian.com>
2016-04-09 18:16:58 +0100

    Setup files

---
+++ Gulpfile.js
@@ -0,0 +1,161 @@
+(function gulpTasks() {
+    'use strict';
+
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	sassLint = require('gulp-sass-lint'),
+	cleanCSS = require('gulp-clean-css'),
+	autoprefixer = require('gulp-autoprefixer'),
+	neat = require('node-neat').includePaths,
+	eslint = require('gulp-eslint'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	sourcemaps = require('gulp-sourcemaps'),
+	sequence = require('gulp-sequence'),
+	notify = require('gulp-notify'),
+	plumber = require('gulp-plumber'),
+	rename = require('gulp-rename'),
+	connect = require('gulp-connect'),
+	queue = require('streamqueue'),
+	paths = {
+		root: 	'./app',
+		js: 	'./assets/js',
+		sass: 	'./assets/sass',
+		mainSASS: './assets/sass/main.sass'
+	},
+	sassVendors = [
+		'./bower_components/font-awesome/scss/'
+	],
+	jsVendors = [
+		'./bower_components/Processing.js/processing.min.js',
+		'./bower_components/webrtc-adapter/adapter-1.0.7.js'
+	];
+
+gulp.task('default', sequence('clean', ['styles','scripts'],'webserver','watch'));
+
+gulp.task('clean',function(){
+
+});
+
+gulp.task('webserver',function(){
+	connect.server({
+		root: paths.root,
+		livereload: true
+	});
+});
+
+gulp.task('watch',function(){
+	gulp.watch(paths.root+"/*.html",['html']);
+	gulp.watch(paths.sass+'/*.sass',['styles']);
+	gulp.watch(paths.js+'/*.js',['scripts']);
+});
+
+gulp.task('html',function(){
+	gulp.src(paths.root+"/*.html")
+		.pipe(connect.reload());
+});
+
+gulp.task('styles', ['sass-lint', 'sass']);
+
+gulp.task('sass-lint',function(){
+	gulp
+		.src(paths.mainSASS)
+		.pipe(plumber({
+			errorHandler: function(err) {
+				notify.onError({
+					title: 'Sass Lint Error',
+					message: '<%= error.message %>',
+					sound: 'Sosumi'
+				})(err);
+				this.emit('end');
+			}
+		}))
+		.pipe(sassLint({'config': 'lint.yml'}))
+		.pipe(sassLint.format());
+		//.pipe(sassLint.failOnError());
+});
+
+gulp.task('sass',function(){
+
+	gulp.src(paths.mainSASS)
+		.pipe(plumber({
+			errorHandler: function(err) {
+				notify.onError({
+					title: 'Sass Compile Error',
+					message: '<%= error.message %>',
+					sound: 'Sosumi'
+				})(err);
+				this.emit('end');
+			}
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			style: 'compact',
+			includePaths: neat,//.concat(sassVendors),
+			sourceMap: true
+		}))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(cleanCSS())
+		.pipe(rename('style.css'))
+		.pipe(gulp.dest(paths.root))
+		.pipe(sourcemaps.write(paths.root))
+		.pipe(gulp.dest(paths.root))
+		.pipe(notify({
+			message: 'SASS complete'
+		}))
+		.pipe(connect.reload());
+});
+
+
+
+gulp.task('scripts', ['es-lint', 'js']);
+
+gulp.task('es-lint',function(){
+	gulp.src(paths.js+'/*.js')
+		.pipe(plumber({
+			errorHandler: function(err) {
+				notify.onError({
+					title: 'JS Compile Error',
+					message: '<%= error.message %>',
+					sound: 'Sosumi'
+				})(err);
+				this.emit('end');
+			}
+		}))
+		.pipe(eslint())
+		.pipe(eslint.format())
+		//.pipe(eslint.failAfterError());
+});
+
+gulp.task('js',function(){
+
+	var concatStream = gulp.src(jsVendors),
+		minStream = gulp.src(paths.js+'/*.js')
+		.pipe(plumber({
+			errorHandler: function(err) {
+				notify.onError({
+					title: 'JS Compile Error',
+					message: '<%= error.message %>',
+					sound: 'Sosumi'
+				})(err);
+				this.emit('end');
+			}
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(uglify());
+
+	return queue({objectMode:true},concatStream,minStream)
+		.pipe(concat('script.min.js'))
+		.pipe(gulp.dest(paths.root))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(paths.root))
+		.pipe(notify({
+			message: 'JS complete'
+		}))
+		.pipe(connect.reload());
+});
+
+})();
--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 > Under construction
 
+##Requires
+
+Browser that supports the [Web Audio API](http://caniuse.com/#search=web%20audio%20api)
+
 ##Project setup
 
 Run the installer in your console (requires NPM and Bower):
